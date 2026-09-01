dc358756ffea1b2f0b81acab0f64b5f955fa7b42
lyniqna <fitriani.widodo18@gmail.com>
2026-09-01 14:23:33 +0700

    assignment no 1

---
+++ Jobsheet2/Assignment/One/Rectangle.java
@@ -0,0 +1,21 @@
+package Jobsheet2.Assignment.One;
+
+public class Rectangle {
+    public int rlong = 0;
+    public int rwide = 0;
+
+    public int getArea() {
+        return rlong * rwide;
+    }
+
+    public int getCircumference() {
+        return 2 * (rlong + rwide);
+    }
+
+    public void displayInfo() {
+        System.out.println("Rectange Long   : " + rlong + " m");
+        System.out.println("Rectangle Wide  : " + rwide + " m");
+        System.out.println("Rectange Area   : " + getArea() + " m^2");
+        System.out.println("Circumference   : " + getCircumference() + " m");
+    }
+}
